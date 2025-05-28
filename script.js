d8c7505ee32f7ac8ad6c5148c962c0a3100bdc6d
byrs00 <byrtusova.stepanka@gmail.com>
2025-05-28 11:10:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const binId = "6836c98a8a456b7966a657de";
 const apiKey = "$2a$10$cEC9UMYDjgPs5bqVeFHXqOU8EaKOGZAnvhz6xxmJ2LX4iGdeAsIrS";
 const url = `https://api.jsonbin.io/v3/b/${binId}/latest`;
 
+let deeds = [];
+
 fetch(url, {
   headers: {
     "X-Master-Key": apiKey
@@ -9,16 +11,19 @@ fetch(url, {
 })
   .then(response => response.json())
   .then(data => {
-    const deeds = data.record.skutky || [];
-    if (deeds.length > 0) {
-      const randomIndex = Math.floor(Math.random() * deeds.length);
-      const randomDeed = deeds[randomIndex];
-      document.getElementById("goodDeedsContainer").textContent = randomDeed;
-    } else {
-      document.getElementById("goodDeedsContainer").textContent = "Žádné skutky nenalezeny.";
-    }
+    deeds = data.record.skutky || [];
   })
   .catch(error => {
     console.error("Chyba při načítání dat:", error);
     document.getElementById("goodDeedsContainer").textContent = "Nepodařilo se načíst data.";
   });
+
+document.getElementById("generateButton").addEventListener("click", () => {
+  if (deeds.length > 0) {
+    const randomIndex = Math.floor(Math.random() * deeds.length);
+    const randomDeed = deeds[randomIndex];
+    document.getElementById("goodDeedsContainer").textContent = randomDeed;
+  } else {
+    document.getElementById("goodDeedsContainer").textContent = "Skutky nejsou dostupné.";
+  }
+});
